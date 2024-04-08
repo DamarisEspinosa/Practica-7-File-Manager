8a005df1247a61f26d8b614e2f3a9cd5441eae10
DamarisEspinosa <2130081@upv.edu.mx>
2024-04-07 21:29:54 -0600

    quitar comentarios

--- logout.php
+++ logout.php
@@ -1,12 +1,9 @@
 <?php
-// Inicia la sesión (si aún no está iniciada)
-session_start();
 
-// Destruye todas las variables de sesión
+session_start();
 session_destroy();
 
-// Redirige al usuario a la página de inicio de sesión (o cualquier otra página deseada)
 header("Location: login.php");
-exit;
+exit();
 
 ?>
