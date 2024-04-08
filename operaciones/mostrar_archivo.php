ddc04e0616710bdafc51499d117694749c1fb9c0
DamarisEspinosa <2130081@upv.edu.mx>
2024-04-08 09:43:13 -0600

    estilo en botones de eliminar y visibilidad

--- operaciones/mostrar_archivo.php
+++ operaciones/mostrar_archivo.php
@@ -7,46 +7,46 @@ require "../config.php";
 
 ?>
 
-<table>
-    <tr>
-        <th>Nombre</th>
-        <th>Descripcción</th>
-        <th>Tamaño de Archivo</th>
-        <th>Enlace</th>
-        
-        <?php if($usuario_admin){ ?>
-        <th>Visible</th>
-        <th>Borrar</th>
-        <?php } ?>
-    </tr>
-
-    <?php 
-
-        include("../conection.php");
-
-        $stmt = $conn->query("SELECT * FROM archivos");
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($result as $valores){
-            $visible = ($valores['es_publico']) ? "Publico" : "No visible";
+<div class="tabla">
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <th>Descripcción</th>
+            <th>Tamaño de Archivo</th>
             
-            if(($valores['es_publico'] || $_SESSION['usuario_esAdmin']) && $valores['usuario_borro_id'] == null){
-                echo "<tr>";
+            <?php if($usuario_admin){ ?>
+            <th>Visible</th>
+            <th>Borrar</th>
+            <?php } ?>
+        </tr>
 
-                echo "<td>". $valores['nombre_archivo'] ."</td>";
-                echo "<td>". $valores['descripcion'] ."</td>";
-                echo "<td>". $valores['tamaño'] ." KB</td>";
-                echo "<td><a href='". "./archivos/".$valores['nombre_archivo_guardado'] ."'>Ver</a></td>";
+        <?php 
 
-                if($usuario_admin){
-                    echo "<td><button onclick=Ver('". $valores['hash_sha256'] ."')>". $visible ."</button></td>";
-                    echo "<td><button onclick=eliminar_archivo('".$valores['nombre_archivo_guardado']."','".$_SESSION['usuario_id']."','".$valores['hash_sha256']."');>Eliminar</button></td>";
+            include("../conection.php");
+
+            $stmt = $conn->query("SELECT * FROM archivos");
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($result as $valores){
+                $visible = ($valores['es_publico']) ? "Publico" : "No visible";
+                
+                if(($valores['es_publico'] || $_SESSION['usuario_esAdmin']) && $valores['usuario_borro_id'] == null){
+                    echo "<tr>";
+                    echo "<td> <a href='". "archivos/".$valores['nombre_archivo_guardado'] ."'>" . $valores['nombre_archivo'] ."</a></td>";
+                    echo "<td>". $valores['descripcion'] ."</td>";
+                    echo "<td>". $valores['tamaño'] ." KB</td>";
+                    //echo "<td><a href='". "archivos/".$valores['nombre_archivo_guardado'] ."'>Ver</a></td>";
+
+                    if($usuario_admin){
+                        echo "<td><button class='botonVB' onclick=Ver('". $valores['hash_sha256'] ."')>". $visible ."</button></td>";
+                        echo "<td><button class='botonVB' onclick=eliminar_archivo('".$valores['nombre_archivo_guardado']."','".$_SESSION['usuario_id']."','".$valores['hash_sha256']."');>Eliminar</button></td>";
+                    }
+
+                    echo "</tr>";
                 }
 
-                echo "</tr>";
             }
+        ?>
 
-        }
-    ?>
-
-</table>
+    </table>
+</div>
